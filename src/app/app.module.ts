fcd53773deea0eedd3571e859bc1e162db9ecbb2
Spider <rushabh6792@gmail.com>
2017-03-25 13:50:42 +0530

    Team created in Firebase DB

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import {RouterModule} from '@angular/router';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
 import { JoinComponentClass} from './components/join/join.component';
@@ -10,6 +11,17 @@ import { IndexComponentClass} from './components/index/index.component';
 import { CreateComponentClass} from './components/create/create.component';
 
 import { appRoutes } from './route';
+
+
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAXJ5PRbI0cPJHh6BS-1pjvaRmRs5ibJYM",
+    authDomain: "rt-code-collab.firebaseapp.com",
+    databaseURL: "https://rt-code-collab.firebaseio.com",
+    storageBucket: "rt-code-collab.appspot.com",
+    messagingSenderId: "89041205883"
+  };
 
 @NgModule({
   declarations: [
@@ -22,7 +34,8 @@ import { appRoutes } from './route';
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot(appRoutes)
+    RouterModule.forRoot(appRoutes),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
 
 @Component({
   selector: '<create-component></create-component>',
@@ -6,4 +7,28 @@ import { Component } from '@angular/core';
    styleUrls: ['./create.component.css']
 })
 export class CreateComponentClass {
+    constructor(private af: AngularFire){
+        // af.database.list('/teams').subscribe(data => {
+        //      console.log(data);
+        // });
+    }
+    create(formData) {
+         if(formData) {
+        console.log(formData);
+        this.af.auth.createUser({
+            email: formData.email,
+            password: formData.passwd
+        }).then(
+            (success) => {
+                const usersList = this.af.database.object('users/' + success.uid);
+                usersList.set(formData)
+                    .then((snap) => {
+                        console.log(snap);
+                    });
+        }).catch(
+            (err) => {
+            console.log(err);
+        });
+        }
+    }
 }
