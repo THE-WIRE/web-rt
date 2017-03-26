201103dfd13fff8f044e4b5c356342d3765970a0
root <root@localhost.localdomain>
2017-03-26 09:09:31 +0530

    Join

--- src/app/components/join/join.component.ts
+++ src/app/components/join/join.component.ts
@@ -1,11 +1,30 @@
 import {Component} from '@angular/core'
+import {nodemailer} from 'nodemailer/lib/nodemailer'
+import {AngularFire, FirebaseListObservable} from 'angularfire2'
+
 
 @Component({
     selector: "<join-component></join-component>",
     templateUrl: './join.component.html'
 })
-
 export class JoinComponentClass {
-    
 
+    // items: FirebaseListObservable<any>;
+    // constructor(af: AngularFire) {
+    //     this.items = af.database.list('/users')
+    // }
+
+    // userExistsCallBack(uid, exists){
+    //     if(exists){
+    //         alert('user' + uid + 'exists!');
+    //     } else {
+    //         alert('user' + uid + 'does not exist!');
+    //     }
+    // }
+
+    // checkIfUserExists(uid){
+    //    if(uid == member_uid){
+        
+    //    }
+    // }
 }
